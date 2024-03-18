3d8e1c7477d3db5040eee75230bfddfba720d3fb
Trần Ngọc Đăng Khôi <khoitranngocdg@gmail.com>
2024-03-18 16:25:12 +0700

    update GET, POST, PUT, DELETE methods

--- controllers/board.js
+++ controllers/board.js
@@ -12,8 +12,6 @@ exports.getAll = async (req, res) => {
 exports.getByOrgId = async (req, res) => {
    try {
       const boards = await Board.find({ orgId: req.params.id });
-      console.log(req.params.id);
-      console.log(boards);
       return res.status(200).json(boards);
    } catch (error) {
       return res.status(500).json({ error: error.message });
@@ -24,6 +22,24 @@ exports.createBoard = async (req, res) => {
    try {
       console.log(req.body);
       const board = await Board.create(req.body);
+      return res.status(200).json(board);
+   } catch (error) {
+      return res.status(500).json({ error: error.message })
+   }
+}
+
+exports.updateBoard = async (req, res) => {
+   try {
+      const board = await Board.updateOne({ _id:req.params.id }, req.body);
+      return res.status(200).json(board);
+   } catch (error) {
+      return res.status(500).json({ error: error.message })
+   }
+}
+
+exports.deleteBoard = async (req, res) => {
+   try {
+      const board = await Board.deleteOne({ _id:req.params.id });
       return res.status(200).json(board);
    } catch (error) {
       return res.status(500).json({ error: error.message })
--- views/board.view.js
+++ views/board.view.js
@@ -1,7 +1,16 @@
-const { getAll, createBoard, getByOrgId } = require("../controllers/board")
+const { getAll, createBoard, getByOrgId, updateBoard, deleteBoard } = require("../controllers/board")
 
 module.exports = function(app){
+    //GET
     app.get('/boards', getAll);
     app.get('/boards/:id', getByOrgId);
+
+    //POST
     app.post('/boards', createBoard);
+
+    //PUT
+    app.put('/boards/:id', updateBoard)
+
+    //DELETE
+    app.delete('/boards/:id', deleteBoard);
 }
